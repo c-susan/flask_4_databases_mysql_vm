ca7af5ebf03a8b9862cb78dcb898d2ce383272cf
Susan Chen <123512714+c-susan@users.noreply.github.com>
2023-10-05 16:46:16 -0400

    Create create_table.sql

---
+++ code/mysql/create_table.sql
@@ -0,0 +1,16 @@
+CREATE TABLE doctors(
+	doctor_id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name TEXT NOT NULL, 
+    last_name TEXT NOT NULL, 
+    department TEXT NOT NULL, 
+    phone_num VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE patients(
+	patient_id INT PRIMARY KEY AUTO_INCREMENT, 
+    first_name TEXT NOT NULL, 
+    last_name TEXT NOT NULL, 
+    date_of_birth DATE,
+    primary_doctor_id INT,
+    FOREIGN KEY (primary_doctor_id) REFERENCES doctors(doctor_id)
+);
