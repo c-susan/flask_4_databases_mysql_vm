6b07de5a147e066ed6fad018343eccf638d659fc
c-susan <susan.chen@stonybrook.edu>
2023-10-06 19:39:48 +0000

    update2

--- code/flask/app.py
+++ code/flask/app.py
@@ -11,15 +11,14 @@ DB_HOST = os.getenv("DB_HOST")
 DB_DATABASE = os.getenv("DB_DATABASE")
 DB_USERNAME = os.getenv("DB_USERNAME")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_PORT = int(os.getenv("DB_PORT", 3306))
-DB_CHARSET = os.getenv("DB_CHARSET", "utf8mb4")
+#DB_PORT = int(os.getenv("DB_PORT", 3306))
+#DB_CHARSET = os.getenv("DB_CHARSET", "utf8mb4")
 
 connect_args={'ssl':{'fake_flag_to_enable_tls': True}}
 connection_string = f'mysql+pymysql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}/{DB_DATABASE}'
 engine = create_engine(
         connection_string,
-        connect_args=connect_args,
-)
+        connect_args=connect_args)
 
 app = Flask(__name__)
 
@@ -28,11 +27,11 @@ def display_data():
     # Establish a database connection
     with engine.connect() as connection:
         # Execute an SQL query to fetch data (replace this with your query)
-        query1 = "SELECT * FROM patients"
-        query2 = "SELECT * FROM doctors"
+        #query1 = 'SELECT * FROM patients'
+        #query2 = 'SELECT * FROM doctors'
 
-        result1 = connection.execute(query1)
-        result2 = connection.execute(query2)
+        result1 = connection.execute(f"""SELECT * FROM patients""")
+        result2 = connection.execute(f"""SELECT * FROM doctors""")
 
         # Fetch all rows of data
         db_data1 = result1.fetchall()
